1b260a0b5bac9c0dba5584b28e48cc665c993175
andreamalvetta <andrea.malvetta@gmail.com>
2019-03-21 17:54:55 +1100

    Change Component1 into Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Component1, Component2, Component3 } from 'ui-kit-test';
+import { Button, Component2, Component3 } from 'ui-kit-test';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Component1>Component 1</Component1>
+        <Button>Click me</Button>
         <Component2>Component 2</Component2>
         <Component3>Component 3</Component3>
       </div>
