85fffc5dad828a52ca442f5a801aa0f4281faf63
andreamalvetta <andrea.malvetta@gmail.com>
2019-07-28 15:09:28 +1000

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import { Button, Component2, Component3 } from 'ui-kit-test';
+import React, { Component } from "react";
+import { Button, Component3 } from "ui-kit-test";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Button>Click me</Button>
-        <Component2>Component 2</Component2>
         <Component3>Component 3</Component3>
       </div>
     );
